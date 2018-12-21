b02f2b660fa28573f839e729ac6c4a6e5fcfe723
Marchal <marchalscott@gmail.com>
2018-12-21 14:26:36 -0700

    added new test file for JavaScript.

---
+++ play.js
@@ -0,0 +1,8 @@
+var name = 'Marchal';
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby){
+    return 'Name is ' + userName + userAge;
+}
+console.log(name);
